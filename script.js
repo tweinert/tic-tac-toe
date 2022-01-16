beddd0d4321d68b868feac25a7cfa4c5372ae94f
tweinert <tait.weinert@outlook.com>
2022-01-17 07:23:04 +1030

    render function started

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
+// modules
 const gameBoard = (() => {
     const gameSquares = new Array(9);
 
     function setSquareMarker(square, marker) {
         gameSquares[square] = marker;
-        console.log(gameSquares[square]);
+    }
+
+    function getSquareMarker(square) {
+        return gameSquares[square];
     }
 
     return {
-        setSquareMarker: setSquareMarker
+        setSquareMarker,
+        getSquareMarker,
     };
 })();
+
+const displayController = (() => {
+    function renderGameBoard() {
+        let boardDiv = document.getElementById("gameBoard");
+        
+        for (let i = 0; i < 9; i++) {
+            boardDiv.textContent += gameBoard.getSquareMarker(i);
+        }
+    }
+
+    return {
+        renderGameBoard,
+    }
+})();
+
+
+
+// factory functions
+const player = () => {
+
+};
+
+
+
+// testing
+for (let i = 0; i < 9; i++) {
+    gameBoard.setSquareMarker(i, "x");
+}
+
+displayController.renderGameBoard();
